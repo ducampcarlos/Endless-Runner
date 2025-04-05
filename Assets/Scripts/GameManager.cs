d8ac083c72de0d77a9da8435e06bbff48ffed24d
ducampcarlos <ducampcarlos@gmail.com>
2025-04-05 11:43:33 -0300

    Added: Condition to start game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -16,6 +17,8 @@ public class GameManager : MonoBehaviour
     [Header("Score")]
     int score;
     [SerializeField] TextMeshProUGUI scoreText; // Reference to the UI text element for displaying the score
+
+    bool gameStarted = false;
 
     private void Awake()
     {
@@ -32,11 +35,34 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        StartGame();
+        scoreText.gameObject.SetActive(false); // Hide the score text at the start
+    }
+
+    private void Update()
+    {
+        if (!gameStarted)
+        {
+            //Using the new input system, when any key is pressed, or the screen is touched, call StartGame
+#if UNITY_ANDROID || UNITY_EDITOR
+            if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.wasPressedThisFrame)
+            {
+                StartGame();
+            }
+#endif
+
+#if UNITY_STANDALONE || UNITY_EDITOR
+            if (Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                StartGame();
+            }
+#endif
+        }
     }
 
     public void StartGame()
     {
+        gameStarted = true;
+        scoreText.gameObject.SetActive(true); // Show the score text when the game starts
         StartCoroutine(SpawnEnemies()); // Start spawning enemies
     }
 
